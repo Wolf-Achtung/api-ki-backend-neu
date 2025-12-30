6e70a8f3f3d3ee50f12e4a6ef894716863f84426
Wolf-Achtung <wolf.hohl@web.de>
2025-12-30 20:17:10 +0100

    Add files via upload

--- routes/auth.py
+++ routes/auth.py
@@ -52,6 +52,8 @@ EMAIL_WHITELIST = {email.lower() for email in [
     "test-v7-final@ki-sicherheit.jetzt",
     "test-v7-1@ki-sicherheit.jetzt",
     "test-v7-2@ki-sicherheit.jetzt",
+    "test-v7-3@ki-sicherheit.jetzt",
+    "test-v7-4@ki-sicherheit.jetzt",
 ]}
 
 router = APIRouter(prefix="/auth", tags=["auth"])
